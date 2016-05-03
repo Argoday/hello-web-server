0310543a899c8d5484462b802db81fe030c8a1f6
Argoday <argoday@argoday.com>
2016-05-03 15:58:09 +0000

    Add ELB commands

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,6 +2,9 @@
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
+
+sudo apt-get install awscli -y
+
 sudo apt-get install ansible -y
 
 sudo ansible-galaxy install nodesource.node
